74504f04043ab80c5599b4ed6cdda7311e04b4e7
IM-Nuthapat <laserbeamna1@gmail.com>
2025-03-05 09:25:00 +0700

    lab7

--- part2/n-tier_architecture/backend/parse-server-api/cloud/functions.js
+++ part2/n-tier_architecture/backend/parse-server-api/cloud/functions.js
@@ -1,3 +1,36 @@
+Parse.Cloud.define("hello", (req) => {
+    req.log.info(req);
+    return "Hi from Parse Server";
+});
+
+Parse.Cloud.define("OnlineAgentByAgentCode", async (request) => {
+    let AgentCode = request.params.AgentCode;
+    let StatusCode = request.params.StatusCode;
+  
+    let returnCode = 0;
+    //------------------
+  
+    const query = new Parse.Query("OnlineAgentLists"); // select * from OnlineAgentLists
+  
+    query.equalTo("AgentCode", AgentCode); // where AgentCode = 'AgentCode'
+  
+    let results;
+  
+    try {
+      results = await query.first();
+  
+      if (results == undefined) {
+        returnCode = "9";
+      } else {
+        returnCode = results.get("AgentStatus");
+      }
+  
+      return returnCode;
+    } catch (error) {
+      throw error.message;
+    }
+});
+
 Parse.Cloud.define("postOnlineAgentListByTeam", async (request) => {
     /*
       Parameter:
